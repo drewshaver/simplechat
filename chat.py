4734d5d1395f7574b1dc01b0596614df618aff3b
Andrew Warshaver <drew@warshaver.com>
2015-07-01 12:33:22 -0400

    v1

---
+++ chat.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template
+from flask.ext.socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TODO'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#TODO better way to do simple forwarding?
+@socketio.on('request population')
+def request_population():
+    emit('register population', broadcast=True)
+
+@socketio.on('register me')
+def register_one(username):
+    emit('register one', username, broadcast=True)
+
+#TODO use rooms so messages aren't public
+@socketio.on('message')
+def send_message(data):
+    emit('message', data, broadcast=True)
+
+#TODO disconnect
+
+#TODO can message yourself?
+
+#TODO sanitize usernames / make sure they are encoded safely
+
+if __name__ == '__main__':
+    socketio.run(app)
